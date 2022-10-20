3dbe094ea658370d8cfb68cdc3986d6fe87eac37
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-20 16:06:59 -0300

    feat: new route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import cors from 'cors'
 import home from './routes/home.js'
+import newuser from './routes/newuser.js'
 
 const port = process.env.PORT || 3333;
 
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/', home)
+app.use('/newuser', newuser)
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
---
+++ routes/newuser.js
@@ -0,0 +1,57 @@
+import express from "express";
+import connection from "../mysql/mysql-connection.js";
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  if (req.body.user === "") {
+    return res.status(401).send({ msg: "O campo 'usuário' está vazio" });
+  }
+
+  if (req.body.email === "") {
+    return res.status(401).send({ msg: "O campo 'e-mail' está vazio" });
+  }
+
+  if (!req.body.password) {
+    return res.status(401).send({ msg: "O campo 'senha' está vazio" });
+  }
+
+  if (!req.body.passCheck) {
+    return res
+      .status(401)
+      .send({ msg: "O campo 'confirme a senha' está vazio" });
+  }
+
+  if (req.body.password !== req.body.passCheck) {
+    res.status(401).send({ msg: "As senha não conferem" });
+    return false;
+  }
+
+  const sql = `INSERT INTO heroku_051d91685db6ea0.user (name, email, password) VALUES ('${req.body.user}', '${req.body.email}' ,'${req.body.password}')`;
+
+  connection.query(
+    sql,
+
+    function (err) {
+      if (err) {
+        if (err.sqlMessage && err.sqlMessage.includes("name")) {
+          res.status(401).send({ msg: "Usuário já existente" });
+          return false;
+        }
+
+        if (err.sqlMessage && err.sqlMessage.includes("email")) {
+          res.status(401).send({ msg: "Email já existente" });
+          return false;
+        }
+
+        res.send(err);
+        return false;
+      }
+
+      console.log("Usuário criado com sucesso");
+      res.send({ msg: "Usuário criado com sucesso" });
+    }
+  );
+});
+
+export default router;
