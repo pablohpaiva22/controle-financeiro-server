2916ddbe8835f35016174d92eb846f50043ba97a
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-20 12:34:17 -0300

    feat: cors and json middlewares confi

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from 'cors'
 import home from './routes/home.js'
 
 const port = process.env.PORT || 3333;
 
 const app = express();
+
+app.use(cors())
+app.use(express.json())
 
 app.use('/', home)
 
