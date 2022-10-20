8d3ebfa41b7c1171bbae3c6f81586fac4f51a0b7
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-20 10:51:07 -0300

    feat: add router - creating home

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express";
+import home from './routes/home.js'
 
 const port = process.env.PORT || 3333;
 
 const app = express();
 
-app.use('/', function(req, res) {
-  res.send({ msg: 'Hello World'})
-})
+app.use('/', home)
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
