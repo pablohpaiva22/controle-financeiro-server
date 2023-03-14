a49ffa09f925229eaa828011b6d35d0b63d0e399
pablohpaiva22 <pablohpaiva22@gmail.com>
2023-03-14 13:55:15 -0300

    fix: removing enabling cors

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,50 +5,48 @@ import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  if (req.method === "OPTIONS") {
-    if (req.body.email === "") {
-      return res.status(401).send({ msg: "O campo 'E-mail' está vazio" });
-    }
-
-    if (req.body.password === "") {
-      return res.status(401).send({ msg: "O campo 'Senha' está vazio" });
-    }
-
-    const sql = `SELECT * FROM railway.users WHERE email = '${req.body.email}'`;
-
-    connection.query(
-      sql,
-
-      function (err, results) {
-        if (err) {
-          res.status(401).send({ msg: "Erro ao acessar banco de dados" });
-          return false;
-        }
-
-        if (results.length === 0) {
-          return res.status(401).send({ msg: "E-mail não cadastrado" });
-        }
-
-        if (req.body.password === results[0].password) {
-          const token = jwt.sign(
-            {
-              id: results[0].id,
-              name: results[0].name,
-              email: results[0].email,
-            },
-            process.env.JWT_KEY,
-            {
-              expiresIn: "1h",
-            }
-          );
-
-          return res.send({ msg: "Usuário logado com sucesso", token });
-        }
-
-        res.status(401).send({ msg: "Senha incorreta" });
-      }
-    );
+  if (req.body.email === "") {
+    return res.status(401).send({ msg: "O campo 'E-mail' está vazio" });
   }
+
+  if (req.body.password === "") {
+    return res.status(401).send({ msg: "O campo 'Senha' está vazio" });
+  }
+
+  const sql = `SELECT * FROM railway.users WHERE email = '${req.body.email}'`;
+
+  connection.query(
+    sql,
+
+    function (err, results) {
+      if (err) {
+        res.status(401).send({ msg: "Erro ao acessar banco de dados" });
+        return false;
+      }
+
+      if (results.length === 0) {
+        return res.status(401).send({ msg: "E-mail não cadastrado" });
+      }
+
+      if (req.body.password === results[0].password) {
+        const token = jwt.sign(
+          {
+            id: results[0].id,
+            name: results[0].name,
+            email: results[0].email,
+          },
+          process.env.JWT_KEY,
+          {
+            expiresIn: "1h",
+          }
+        );
+
+        return res.send({ msg: "Usuário logado com sucesso", token });
+      }
+
+      res.status(401).send({ msg: "Senha incorreta" });
+    }
+  );
 });
 
 export default router;
